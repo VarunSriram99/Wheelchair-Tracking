dd1b26ef3c2c4f213222b3adf45a905d639d8c7a
VarunSriram99 <47653646+VarunSriram99@users.noreply.github.com>
2021-01-19 19:36:56 +0530

    Add files via upload

---
+++ WiFi_Strength/WiFi_Strength.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+
+char ssid[] = "Subramanyam";
+//password of your WPA Network
+char pass[] = "robinson";
+
+void setup()
+{
+ WiFi.begin(ssid, pass);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  }
+  // if you are connected, print out info about the connection:
+  else {
+   // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.println(rssi);
+  }
+}
+
+void loop () {}
